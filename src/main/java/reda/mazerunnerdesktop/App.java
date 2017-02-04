e4aea8d55ecf09c88c66fec7d75c27dc56bf3527
mrMekdad <mekdadr6@gmail.com>
2017-02-04 08:06:00 +0100

    maze-runner-desktop: introduce core source layout

---
+++ src/main/java/reda/mazerunnerdesktop/App.java
@@ -0,0 +1,8 @@
+package reda.mazerunnerdesktop;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Maze Runner Desktop");
+        System.out.println("Author: Red@");
+    }
+}
---
+++ src/main/java/reda/mazerunnerdesktop/Reporter.java
@@ -0,0 +1,7 @@
+package reda.mazerunnerdesktop;
+
+public class Reporter {
+    public String summary() {
+        return "Maze Runner Desktop maintained by Red@";
+    }
+}
